90c3ce6edf7f47d75703cbe06bfc69988fcc858b
fhassis <fhassis2@gmail.com>
2021-06-18 16:13:24 -0300

    changed token creation mechanism by username.

--- src/utils/jwt.py
+++ src/utils/jwt.py
@@ -1,8 +1,14 @@
 from hmac import new
 from hashlib import sha256
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from base64 import urlsafe_b64encode, urlsafe_b64decode
 from orjson import loads, dumps
+from typing import TypedDict
+
+
+class Payload(TypedDict):
+    sub: str    # subject: username
+    exp: int    # expiration time, in UTC unix time
 
 
 class JWT(object):
@@ -16,7 +22,9 @@ class JWT(object):
         self.secret_key = secret_key.encode()
         self.b64_header = urlsafe_b64encode(dumps({'typ': 'JWT', 'alg': 'HS256'})).decode('utf-8')
 
-    def create_token(self, payload: dict) -> str:
+    def create_token(self, username: str) -> str:
+        exp_time = int((datetime.now(timezone.utc) + timedelta(days=7)).timestamp())
+        payload: Payload = dict(sub=username, exp=exp_time)
         b64_payload = urlsafe_b64encode(dumps(payload)).decode('utf-8')
         b64_signature = self._generate_signature(self.b64_header, b64_payload)
         return f'{self.b64_header}.{b64_payload}.{b64_signature}'
@@ -26,7 +34,7 @@ class JWT(object):
         b64_signature_checker = self._generate_signature(b64_header, b64_payload)
 
         # load payload to check the field 'exp'
-        payload = loads(urlsafe_b64decode(b64_payload))
+        payload: Payload = loads(urlsafe_b64decode(b64_payload))
         unix_time_now = datetime.now(timezone.utc).timestamp()
 
         if payload.get('exp') and payload['exp'] < unix_time_now:
@@ -47,20 +55,18 @@ class JWT(object):
         ).decode('utf-8')
 
 
-# if __name__ == '__main__':
-#
-#     from datetime import timedelta
-#
-#     payload = {
-#         'userId': '55395427-265a-4166-ac93-da6879edb57a',
-#         'exp': (datetime.now() + timedelta(minutes=5)).timestamp(),
-#     }
-#     secret_key = '52d3f853c19f8b63c0918c126422aa2d99b1aef33ec63d41dea4fadf19406e54'
-#
-#     jwt = JWT(secret_key)
-#
-#     token = jwt.create_token(payload)
-#     print(token)
-#
-#     decoded_token = jwt.validate_token(token)
-#     print(decoded_token)
+if __name__ == '__main__':
+
+    username = 'someusername'
+    secret_key = '52d3f853c19f8b63c0918c126422aa2d99b1aef33ec63d41dea4fadf19406e54'
+
+    # create jwt object
+    jwt = JWT(secret_key)
+
+    # test token creation
+    token = jwt.create_token(username)
+    print(token)
+
+    # test token validation
+    decoded_token = jwt.validate_token(token)
+    print(decoded_token)
