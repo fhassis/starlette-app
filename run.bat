d2cb02c8ad6296e2b896f4618378d9e1733eb0b9
fhassis <fhassis2@gmail.com>
2021-09-08 18:47:49 -0300

    Added option to run either with uvicorn or hypercorn with ssl.

--- run.bat
+++ run.bat
@@ -1,3 +1,7 @@
-pipenv run python -m uvicorn src.main:app
+pipenv run python -m uvicorn src.main:app --host 127.0.0.1 --port 8000 
+REM pipenv run python -m uvicorn src.main:app --host 127.0.0.1 --port 8000 --ssl-keyfile ./ssl/certificates/key.pem --ssl-certfile ./ssl/certificates/cert.pem
+
+REM pipenv run python -m hypercorn src.main:app --bind "0.0.0.0:8000"
+REM pipenv run python -m hypercorn src.main:app --bind "0.0.0.0:8000" --keyfile ./ssl/certificates/key.pem --certfile ./ssl/certificates/cert.pem
 
 pause
