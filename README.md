e5d8dbebdef04a599ba41f4470572258f6d4f14c
fhassis <fhassis2@gmail.com>
2021-10-31 13:34:40 +0000

    Added support for HTTP3 with Hypercorn.

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ This is a template for a Starlette Python server application.
 
 ## Main Features
 
-- Hypercorn serving over HTTP/2
+- Hypercorn serving over HTTP/2 and HTTP/3
+- Uvicorn over HTTP/1 behind a proxy
 - Starlette ASGI application
 - ORJSON responses
 - JWT authentication
--- run.bat
+++ run.bat
@@ -1,5 +1,18 @@
-pipenv run python -m uvicorn src.main:app --host 127.0.0.1 --port 8000
-REM pipenv run python -m uvicorn src.main:app --host 127.0.0.1 --port 8000 --ssl-keyfile ./ssl/certificates/key.pem --ssl-certfile ./ssl/certificates/cert.pem
+@echo off
 
-REM pipenv run python -m hypercorn src.main:app --bind "0.0.0.0:8000"
-REM pipenv run python -m hypercorn src.main:app --bind "0.0.0.0:8000" --keyfile ./ssl/certificates/key.pem --certfile ./ssl/certificates/cert.pem
+SET HOST=0.0.0.0
+SET PORT=8000
+
+SET CERTFILE=./ssl/certificates/cert.pem
+SET KEYFILE=./ssl/certificates/key.pem
+
+REM SET CERTFILE=C:\Certbot\live\fhassis.ddns.net\fullchain.pem
+REM SET KEYFILE=C:\Certbot\live\fhassis.ddns.net\privkey.pem
+
+REM pipenv run python -m uvicorn src.main:app --host %HOST% --port %PORT% --proxy-headers
+REM pipenv run python -m uvicorn src.main:app --host %HOST% --port %PORT% --ssl-keyfile %KEYFILE% --ssl-certfile %CERTFILE%
+
+REM pipenv run python -m hypercorn src.main:app --bind %HOST%:%PORT%
+REM pipenv run python -m hypercorn src.main:app --bind %HOST%:%PORT% --keyfile %KEYFILE% --certfile %CERTFILE%
+
+pipenv run python -m hypercorn src.main:app --bind %HOST%:%PORT% --quic-bind "0.0.0.0:4443" --keyfile %KEYFILE% --certfile %CERTFILE%
